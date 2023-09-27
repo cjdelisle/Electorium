5a071358232714710f03cf09e7a95edfa69c0924
Caleb James DeLisle <cjd@cjdns.fr>
2023-09-27 16:07:54 +0200

    AFL run.sh no UI for worker nodes

--- fuzz/run.sh
+++ fuzz/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 for i in $(seq 1 $(nproc)); do
     echo "Launch $i"
-    afl-fuzz -S "fuzzer-$i" -i ./fuzz/inputs/ -o ./fuzz-outputs/ -- ./target/release/fuzz
+    AFL_NO_UI=1 afl-fuzz -S "fuzzer-$i" -i ./fuzz/inputs/ -o ./fuzz-outputs/ -- ./target/release/fuzz &
 done
